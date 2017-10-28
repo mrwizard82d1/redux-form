ce1a52ad5f3a9eb84654610b68b457f23124cded
Larry Jones <larryj@blinds.com>
2017-10-27 19:50:28 -0500

    Add last name, email and sex

--- simple-form/src/SimpleForm.js
+++ simple-form/src/SimpleForm.js
@@ -9,6 +9,31 @@ const SimpleForm = ({ handleSubmit }) => (
         <Field name="firstName" component="input" type="text" placeholder="First Name" />
       </div>
     </div>
+    <div>
+      <label>Last Name</label>
+      <div>
+        <Field name="lastName" component="input" type="text" placeholder="Last Name" />
+      </div>
+    </div>
+    <div>
+      <label>Email</label>
+      <div>
+        <Field name="email" component="input" type="email" placeholder="Email" />
+      </div>
+    </div>
+    <div>
+      <label>Sex</label>
+      <div>
+        <label>
+          <Field name="sex" component="input" type="radio" value="male" />{' '}
+          Male
+        </label>
+        <label>
+          <Field name="sex" component="input" type="radio" value="female" />{' '}
+          Female
+        </label>
+      </div>
+    </div>
   </form>
 );
 
