d5a0280716b76f6af2aee7f0324e8f354c95de1e
Larry Jones <larryj@blinds.com>
2017-10-28 09:02:43 -0500

    Complete synchronous validation.

--- sync-validation/src/SyncValidationForm.js
+++ sync-validation/src/SyncValidationForm.js
@@ -1,6 +1,41 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
+function validate(values) {
+  const errors = {};
+  if (!values.username) {
+    errors.username = 'Required';
+  } else if (values.username.length > 15) {
+    errors.username = 'Must be 15 characters or less';
+  }
+  
+  if (!values.email) {
+    errors.email = 'Required'
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address';
+  }
+  
+  if (!values.age) {
+    errors.age = 'Required';
+  } else if (isNaN(Number(values.age))) {
+    errors.age = 'Must be a number';
+  } else if (Number(values.age) < 18) {
+    errors.age = 'Sorry, you must be at least 18 years old';
+  }
+  
+  return errors;
+}
+
+function warn(values) {
+  const warnings = {};
+  
+  if (values.age < 19) {
+    warnings.age = 'Hmmm. You seem a bit young...';
+  }
+  
+  return warnings;
+}
+
 function renderField({ input, label, type, meta : { touched, error, warning }}) {
   return (
     <div>
@@ -35,5 +70,7 @@ function SyncValidationForm({ handleSubmit, pristine, reset, submitting }) {
 }
 
 export default reduxForm({
-  form: 'sync-validation'
+  form: 'sync-validation', // a unique identifier for this form
+  validate, // validation function used by redux-form
+  warn, // warning function used by redux-form
 })(SyncValidationForm);
