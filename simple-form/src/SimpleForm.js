d7e501276278a09a0238af5c29da882a4220dd3f
Larry Jones <larryj@blinds.com>
2017-10-27 19:30:45 -0500

    A single field on the simple form

--- simple-form/src/SimpleForm.js
+++ simple-form/src/SimpleForm.js
@@ -3,10 +3,16 @@ import { Field, reduxForm } from 'redux-form';
 
 const SimpleForm = ({ handleSubmit }) => (
   <form onSubmit={handleSubmit}>
-    <p>
-      To submit, or not to submit
-    </p>
+    <div>
+      <label>First Name</label>
+      <div>
+        <Field name="firstName" component="input" type="text" placeholder="First Name" />
+      </div>
+    </div>
   </form>
 );
 
-export default SimpleForm;
+export default reduxForm({
+  form: 'simple', // a unique identifier for this form
+})(SimpleForm);
+
