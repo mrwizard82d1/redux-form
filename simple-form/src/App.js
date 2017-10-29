e8526dc72af9a53db1ea83ed7e21b58bced08e19
Larry Jones <larryj@blinds.com>
2017-10-29 15:04:59 -0500

    Add showResults to simple form validation

--- simple-form/src/App.js
+++ simple-form/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SimpleForm from './SimpleForm';
+import showResults from './showResults';
 
 class App extends Component {
   render() {
@@ -11,7 +12,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <SimpleForm />
+        <SimpleForm onSubmit={showResults} />
       </div>
     );
   }
